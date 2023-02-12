8abec71225e8dc4562cedf92cf81ec542d983555
Vitalik0422 <vitaliy19980422@gmail.com>
2023-02-12 13:03:08 +0200

    addFunc

--- js/main.js
+++ js/main.js
@@ -3,65 +3,115 @@ const trafficLightsTopleft = document.querySelector('.trafficLightsTopleft')
 const trafficLightsBottomleft = document.querySelector('.trafficLightsBottomleft')
 const trafficLightsBottomrigth = document.querySelector('.trafficLightsBottomrigth')
 const btn = document.querySelector('.btn')
-    // trafficLightsTopRight.childNodes[3].classList.add('green')
-    // // trafficLightsTopleft.childNodes[3].classList.add('yellow')
-    // // trafficLightsBottomleft.childNodes[3].classList.add('green')
-    // trafficLightsBottomrigth.childNodes[3].classList.add('green')
 
-    btn.addEventListener('click', () => {
-        ligthRedFirst()   
-        setTimeout(ligthGreenSecondary, 500)
-    });
+btn.addEventListener('click', () => {
+    ligthRedFirstGroup()   
+    ligthGreenSecondaryGroup()
+});
+
+const ligthRedFirstGroup = () => {
+    ligthRedFirstGroupTrafficOne()
+    ligthRedFirstGroupTrafficTwo()
+    setTimeout(ligthGreenFirstGroup, 8000)
+}
+
+const ligthGreenFirstGroup = () => {
+    ligthGreenFirstGroupTrafficOne()
+    ligthGreenFirstGroupTrafficTwo()
+    setTimeout(ligthYellowFirstGroup,5500)
+}
+
+const ligthYellowFirstGroup = () => {
+    ligthYellowFirstGroupTrafficOne()
+    ligthYellowFirstGroupTrafficTwo()
+    setTimeout(ligthRedFirstGroup, 2500)
+}
+
+///2 група 
+const ligthRedSecondaryGroup = () => {
+    ligthRedSecondaryGroupTrafficOne()
+    ligthRedSecondaryGroupTrafficTwo()
+    setTimeout(ligthGreenSecondaryGroup, 8000)
+}
+
+const ligthGreenSecondaryGroup = () => {
+    ligthGreenSecondaryGroupTrafficOne()
+    ligthGreenSecondaryGroupTrafficTwo()
+    setTimeout(ligthYellowSecondaryGroup,5500)
+}
+
+const ligthYellowSecondaryGroup = () => {
+    ligthYellowSecondaryGroupTrafficOne()
+    ligthYellowSecondaryGroupTrafficTwo()
+    setTimeout(ligthRedSecondaryGroup, 2500)
+}
+
+
 
 
 //Світлофори першої групи
-const ligthRedFirst = () =>{
+const ligthRedFirstGroupTrafficOne = () =>{
     trafficLightsTopRight.childNodes[1].classList.add('red')
     trafficLightsTopRight.childNodes[5].classList.remove('green')
     trafficLightsTopRight.childNodes[3].classList.remove('yellow')
+}
+
+const ligthRedFirstGroupTrafficTwo = () => {
     trafficLightsBottomrigth.childNodes[1].classList.add('red')
     trafficLightsBottomrigth.childNodes[5].classList.remove('green')
     trafficLightsBottomrigth.childNodes[3].classList.remove('yellow')
-    setTimeout(ligthGreenFirst, 8000)
 
 }
-const ligthGreenFirst = () => {
+const ligthGreenFirstGroupTrafficOne = () => {
     trafficLightsTopRight.childNodes[1].classList.remove('red')
     trafficLightsTopRight.childNodes[5].classList.add('green')
+}
+    
+const ligthGreenFirstGroupTrafficTwo = () => {
     trafficLightsBottomrigth.childNodes[1].classList.remove('red')
     trafficLightsBottomrigth.childNodes[5].classList.add('green')
-    setTimeout(ligthYellowFirst, 4000)
-
 }
-const ligthYellowFirst = () => {
+
+    const ligthYellowFirstGroupTrafficOne = () => {
     trafficLightsTopRight.childNodes[3].classList.add('yellow')
+    trafficLightsTopRight.childNodes[5].classList.remove('green')
+}
+    const ligthYellowFirstGroupTrafficTwo = () => {
     trafficLightsBottomrigth.childNodes[3].classList.add('yellow')
-    setTimeout(ligthRedFirst, 2500)
+    trafficLightsBottomrigth.childNodes[5].classList.remove('green')
 }
 
 //світлофори 2 групи
 
-const ligthRedSecondary = () =>{
+const ligthRedSecondaryGroupTrafficOne = () =>{
     trafficLightsTopleft.childNodes[1].classList.add('red')
     trafficLightsTopleft.childNodes[5].classList.remove('green')
     trafficLightsTopleft.childNodes[3].classList.remove('yellow')
+}
+
+const ligthRedSecondaryGroupTrafficTwo = () => {
     trafficLightsBottomleft.childNodes[1].classList.add('red')
     trafficLightsBottomleft.childNodes[5].classList.remove('green')
     trafficLightsBottomleft.childNodes[3].classList.remove('yellow')
-    setTimeout(ligthGreenSecondary, 8000)
-
 }
-const ligthGreenSecondary = () => {
+
+const ligthGreenSecondaryGroupTrafficOne = () => {
     trafficLightsTopleft.childNodes[1].classList.remove('red')
     trafficLightsTopleft.childNodes[5].classList.add('green')
+}
+
+const ligthGreenSecondaryGroupTrafficTwo = () => {
     trafficLightsBottomleft.childNodes[1].classList.remove('red')
     trafficLightsBottomleft.childNodes[5].classList.add('green')
-    setTimeout(ligthYellowSecondary, 4000)
-
 }
-const ligthYellowSecondary = () => {
+
+const ligthYellowSecondaryGroupTrafficOne = () => {
     trafficLightsTopleft.childNodes[3].classList.add('yellow')
+    trafficLightsTopleft.childNodes[5].classList.remove('green')
+}
+
+ligthYellowSecondaryGroupTrafficTwo = () => {
     trafficLightsBottomleft.childNodes[3].classList.add('yellow')
-    setTimeout(ligthRedSecondary, 2500)
+    trafficLightsBottomleft.childNodes[5].classList.remove('green')
 }
 
